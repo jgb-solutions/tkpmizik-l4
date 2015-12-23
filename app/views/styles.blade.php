23df7bdb66283837a318c19f17569556b346a81f
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2015-12-23 18:33:06 -0500

    Quick Commit

--- app/views/styles.blade.php
+++ app/views/styles.blade.php
@@ -3,4 +3,4 @@
 <link type="text/css" href="/css/lib/360player-visualization.css" rel="stylesheet">
 <link type="text/css" href="/css/lib/font-awesome.min.css" rel="stylesheet">
 <link type="text/css" href="/css/source/style.css" rel="stylesheet">
-{{-- <link rel="stylesheet" type="text/css" href="/css/build/compile.min.css"> --}}
+ --}}{{-- <link rel="stylesheet" type="text/css" href="/css/build/compile.min.css">
