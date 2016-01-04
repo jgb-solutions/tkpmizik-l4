ef792d8c048e3392a9b62ecf5d46e17b6da72e73
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-01-04 16:43:28 -0500

    Quick Commit

--- app/views/mp4/video-player.blade.php
+++ app/views/mp4/video-player.blade.php
@@ -1,8 +1,10 @@
-<div class="embed-responsive embed-responsive-16by9">
-  <iframe
-  	class="embed-responsive-item"
-  	src="https://www.youtube.com/embed/{{ $mp4->youtube_id }}">
-  </iframe>
+<div class="row">
+	<div class="embed-responsive embed-responsive-16by9">
+	  <iframe
+	  	class="embed-responsive-item"
+	  	src="https://www.youtube.com/embed/{{ $mp4->youtube_id }}">
+	  </iframe>
+	</div>
 </div>
 
 <hr>
