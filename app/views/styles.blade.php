99541032c3bc2004887d123ccc9beab6310dcd03
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-01-13 17:12:49 -0500

    Quick Commit

--- app/views/styles.blade.php
+++ app/views/styles.blade.php
@@ -4,4 +4,5 @@
 <link type="text/css" href="/css/lib/font-awesome.min.css" rel="stylesheet">
 <link type="text/css" href="/css/source/style.css" rel="stylesheet"> --}}
 
-<link rel="stylesheet" type="text/css" href="/css/build/compile.min.css">
+{{-- <link rel="stylesheet" type="text/css" href="/css/build/compile.min.css"> --}}
+<link rel="stylesheet" type="text/css" href="http://tkpmizik.jgbcdn.ml/css/build/compile.min.css">
