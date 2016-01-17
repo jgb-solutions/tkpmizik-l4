b4f22db04c42198efff30e2da178172fb3496318
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-01-16 21:34:48 -0500

    Quick Commit

--- app/views/styles.blade.php
+++ app/views/styles.blade.php
@@ -2,6 +2,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ TKPM::asset('css/all.css') }}">
 @else
 	<link rel="stylesheet" type="text/css" href="{{ TKPM::asset('css/app.css') }}">
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link
+		rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 @endif
