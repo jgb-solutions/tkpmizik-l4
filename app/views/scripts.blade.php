f13d8ea04e840b9d575cb0914811558183ac9505
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-01-18 18:20:31 -0500

    Quick Commit

--- app/views/scripts.blade.php
+++ app/views/scripts.blade.php
@@ -1,1 +1,12 @@
 <script src="{{ TKPM::asset('js/app.js') }}"></script>
+
+{{-- Facebook Page Plugin --}}
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=794106764051904";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
--- app/views/sidebar.blade.php
+++ app/views/sidebar.blade.php
@@ -1,7 +1,12 @@
 <div class="col-sm-4 bg-info padding1" id="sidebar">
 
+	@include('inc.modules.facebook')
+
+	<hr>
+
 	@include('modules.categories')
 
 	@include('inc.ads.sidebar')
+
 
 </div>
