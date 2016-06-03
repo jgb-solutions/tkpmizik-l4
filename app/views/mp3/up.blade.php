fc877230991533c7f642284554f762af57591c2a
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-06-02 20:07:45 -0500

    Quick Commit

--- app/views/mp3/up.blade.php
+++ app/views/mp3/up.blade.php
@@ -21,7 +21,7 @@
 		@include('inc.errors')
 
 
-		{{ Form::open(['method' => 'POST', 'url' => '/mp3', 'files' => true, 'class' => 'form-horizontal', 'id' => 'upFormm']) }}
+		{{ Form::open(['method' => 'POST', 'url' => '/mp3', 'files' => true, 'class' => 'form-horizontal', 'id' => 'upForm']) }}
 
 			@if ( Auth::user() )
 				@include('inc.free-paid')
